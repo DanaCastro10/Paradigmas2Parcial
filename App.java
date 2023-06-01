82d166464af1b7fb6e6add244f2be295d0f9fff5
DanaCastro10 <124329973+DanaCastro10@users.noreply.github.com>
2023-05-31 23:27:08 -0600

    Herencia

---
+++ App.java
@@ -0,0 +1,13 @@
+package com.pp2;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Student e= new Student(26,"Kevin","MIT");
+		e.showAge();
+
+	}
+
+}
