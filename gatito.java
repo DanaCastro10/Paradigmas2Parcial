5aa68e88002851a7e7ebede2797ce00ba8a51a2a
DanaCastro10 <124329973+DanaCastro10@users.noreply.github.com>
2023-05-31 23:23:28 -0600

    Lección 2.2

---
+++ gatito.java
@@ -0,0 +1,25 @@
+package pp2;
+
+public class gatito {
+
+	
+	public static void drawLegs () {
+		
+	
+		System.out.println("       ||       ||     ");
+		System.out.println("       ||       ||     ");
+		System.out.println("      (||)     (||)    ");
+		
+		
+	}
+		public static void main(String[] args) {
+		    System.out.println("   /\\           /\\  ");
+		  System.out.println("  /  \\_________/  \\ ");
+		System.out.println(" /                 \\ ");
+		 System.out.println("(  /\\          /\\  ");
+	     System.out.println("====	   v 	  ====");
+		 System.out.println("=======(___|___)=======");
+		 System.out.println("   (               )   ");
+		 System.out.println("    (_____________)");
+drawLegs();
+}}
