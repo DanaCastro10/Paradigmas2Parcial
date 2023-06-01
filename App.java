d69bdaf031d7528962faecea90aad89f8f0af96e
DanaCastro10 <124329973+DanaCastro10@users.noreply.github.com>
2023-06-01 00:10:48 -0600

    Multithreading

--- App.java
+++ App.java
@@ -5,8 +5,11 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Student e= new Student(26,"Kevin","MIT");
-		e.showAge();
+		Thread t1= new Thread(new Runner1());
+		Thread t2= new Thread(new Runner1());
+		
+		t1.start();
+		t2.start();
 
 	}
 
